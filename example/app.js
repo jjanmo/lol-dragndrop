2f6642a11bd1d2700463cd520eea8974d9a2ab46
jjanmo <jjanmo07@hanmail.net>
2021-03-03 23:39:19 +0900

    Add drag&drop-todoapp style

--- example/app.js
+++ example/app.js
@@ -1,11 +1,3 @@
-/*
-event handler
-ondragstart : 사용자가 요소나 텍스트 블록을 드래그하기 시작했을 때 발생
-ondragover : 요소나 텍스트 블록을 적합한 드롭 대상 위로 지나갈 때 발생
-ondrop : 요소나 텍스트를 드래그 할 때 발생
-
-*/
-
 const $draggable = document.querySelector("#draggable");
 const $dropzone = document.querySelector(".dropzone");
 
